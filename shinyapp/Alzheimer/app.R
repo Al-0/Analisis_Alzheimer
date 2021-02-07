73984d114b1cfe103f6bfed2ed85c2c8b0acfe92
Al-0-Quickapps <avalenciana@quickapps.mx>
2021-02-07 15:16:35 -0600

    Imágenes y OASIS dataset

--- shinyapp/Alzheimer/app.R
+++ shinyapp/Alzheimer/app.R
@@ -13,6 +13,21 @@ library(shinythemes)
 
 # Define UI for application
 ui <- fluidPage(
+    # Estilo CSS, utilizado para reparar cierto aspectos que hacian el texto díficil de leer.
+    tags$style("
+        .nav-tabs {
+            background-color: #85929E;
+        }
+        
+        .nav-tabs-custom .nav-tabs li.active:hover a, .nav-tabs-custom .nav-tabs li.active a {
+            background-color: transparent;
+            border-color: transparent;
+        }
+        
+        .nav-tabs-custom .nav-tabs li.active {
+        border-top-color: #FFF;
+    }"
+    ),
     titlePanel("Enfermedad de Alzheimer. Análisis y modelado usando los datasets OASIS."),
     theme = shinytheme("superhero"),
     #shinythemes::themeSelector(), 
@@ -94,15 +109,43 @@ ui <- fluidPage(
                         br(),
                         h4("Debido a que los síntomas iniciales que preceden la enfermedad suelen tomar años en presentarse, la enfermedad de Alzheimer es comúnmente identificada en adultos mayores. Un sistema que permita el diagnóstico temprano de la enfermedad facilitaría tanto las intervenciones de tratamiento como el planeamiento familiar para el cuidado digno del paciente."),
                         h4("Desafortunadamente, el diagnóstico diferencial de la enfermedad de Alzheimer es un proceso intensivo, el cual consume mucho tiempo y es costoso, implicando el desarrollo de exámenes físicos y mentales, pruebas neurológicas y de laboratorio, así como la toma de imagenes cerebrales (como son la imágen por resonancia magnética - MRI, tomografía computarizada - CT, y tomografía por emisión de positrones - PET)."),
-                        h4("Por lo anterior, sería conveniente tener un sistema que, si bien no diagnostique a una persona con un desorden neurodegenerativo (este tipo de diagnósticos jamás debe ser automatizado, siempre debe pasar por el juicio y criterio de un especialista humano), permita indicar de forma tentativa la posibilidad de que el paciente desarrolle en un futuro este desorden, facilitando así un poco el proceso de seguimiento de la enfermedad en los pacientes.")
+                        h4("Por lo anterior, sería conveniente tener un sistema que, si bien no diagnostique a una persona con un desorden neurodegenerativo (este tipo de diagnósticos jamás debe ser automatizado, siempre debe pasar por el juicio y criterio de un especialista humano), permita indicar de forma tentativa la posibilidad de que el paciente desarrolle en un futuro este desorden, facilitando así un poco el proceso de seguimiento de la enfermedad en los pacientes."),
+                        img(src = "BrainMRI3planes.gif", height = 200, width = 440, style="display: block; margin-left: auto; margin-right: auto;")
+                    )
+                ),
+                tabItem(
+                    tabName = "mex",
+                    fluidRow(
                         
                     )
                 ),
                 tabItem(
-                    tabName = "data-table",
+                    tabName = "oasis",
                     fluidRow(
-                        titlePanel(h3("Tabla de Datos de Match.Data", align = "center")),
-                        dataTableOutput("data_table")
+                        tabsetPanel(
+                            tabPanel(
+                                title = "OASIS",
+                                titlePanel(h1("OASIS Brains project", align = "center")),
+                                br(),
+                                h4("La serie de acceso abierto de estudios de imágenes (Open Access Series of Imaging Studies - OASIS) es una serie de datos de neuroimágenes abierto a todo público para su estudio y análisis."),
+                                h4("La serie consiste de 3 conjuntos de datos distintos. La primera serie, denomianada OASIS-1, es un estudio transversal, lo cuál signifiva que es un estudio observacional y descriptivo que mide la prevalencia de la exposición y del efecto en una muestra poblacional en un sólo momento. Dicho de otra manera, este estudio recolecta la información de diversas visitas por individuos distintos, teniendo cada individuo una sóla visita."),
+                                h4("El segundo conjunto de datos, denominado OASIS-2, es por su parte un estudio longitudinal, es decir, un estudio donde se evalúa a un grupo de personas repetidamente a lo largo de varios años. Este conjunto tiene una cantidad menor de participantes, sin embargo cada paciente es examinada 2 o más veces(hasta 5), lo cuál nos permite realizar un estudio del desarrollo de la enfermedad a lo largo de los años."),
+                                br(),
+                                img(src = "oasis-logo.png", height = 200, width = 440, style="display: block; margin-left: auto; margin-right: auto;"),
+                                br(),
+                                h2("Variables")
+                            ),
+                            tabPanel(
+                                title = "OASIS 1: Cross-sectional",
+                                titlePanel(h1("OASIS-1: Cross-sectional MRI Data in Young, Middle Aged, Nondemented and Demented Older Adults", align = "center")),
+                                dataTableOutput("sectional_table")
+                            ),
+                            tabPanel(
+                                title = "OASIS 2: Longitudinal",
+                                titlePanel(h1("OASIS-2: Longitudinal MRI Data in Nondemented and Demented Older Adults", align = "center")),
+                                dataTableOutput("longitudinal_table")
+                            )
+                        )
                     )
                 ),
                 tabItem(
@@ -143,7 +186,19 @@ server <- function(input, output) {
     library(ggplot2)
     library(dplyr)
     
+    # Los datasets de OASIS utilizados para desarrollar el proyecto
+    sectional <- read.csv("oasis_cross-sectional.csv")
+    longitudinal <- read.csv("oasis_longitudinal.csv")
     
+    # Envia los datasets utilizados integramente al frontend y los muestra en una data table
+    output$sectional_table <- renderDataTable(
+        {sectional},
+        options = list(aLengthMenu = c(10, 20, 50), iDisplayLength = 10)
+    )
+    output$longitudinal_table <- renderDataTable(
+        {longitudinal},
+        options = list(aLengthMenu = c(10, 20, 50), iDisplayLength = 10)
+    )
     
     output$plot1 <- renderPlot({
         
@@ -170,11 +225,6 @@ server <- function(input, output) {
             ylim(0,50)
     })
     
-    
-    output$data_table <- renderDataTable(
-        {match.data},
-        options = list(aLengthMenu = c(20, 50, 100, 500, 1000), iDisplayLength = 20)
-    )
 }
 
 # Run the application 
