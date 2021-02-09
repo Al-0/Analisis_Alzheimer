bbf015f1a2695f53551edb7386a298a2f152e6ea
Al-0-Quickapps <avalenciana@quickapps.mx>
2021-02-08 21:32:12 -0600

    Fin del desarrollo del SVM

--- Modelo_predictor_SVM.R
+++ Modelo_predictor_SVM.R
@@ -35,191 +35,68 @@ svmfit <- svm(Demented~MMSE + Age, data = dat2, kernel = "linear",
               cost = 10, scale = FALSE)
 summary(svmfit)
 svmfit$index
-plot(svmfit, dat2, ylim=c(14,31), pch = 15)
+plot(svmfit, dat2, ylim=c(14,31))
 # Como podemos apreciar, este modelo simple sólo puede dividir nuestro dataset con
-# una simple linea. Aun con el mejor esfuerzo
+# una simple linea. Aun con el mejor esfuerzo no es muy bueno clasificando a los pacientes
+# (aun así, mejor que el modelo lineal). Es necesario seguir trabajando para obtener un mejor
+# modelo de predicción.
 
-# Acontinuación, mostramos el clasificador de vectores de soporte junto con las observaciones. Los vectores de soporte se muestran como $x's$
-
-###
-
-
-plot(svmfit, dat)
-
-
-###
-
-# También podemos observar los índices (números de filas en el data frame) que corresponden a vectores de soporte
-
-
-svmfit$index
-
-
-
-length(svmfit$index)
-
-
-###
-
-# Mostramos un breve resumen del ajuste
-
-summary(svmfit)
-
-
-###
-
-# Volvemos a realizar el ajuste pero ahora con el valor del parámetro `cost = 0.1`
-
-
-svmfit <- svm(y~., data = dat, kernel = "linear", 
-              cost = 0.1, scale = FALSE)
-
-
-###
-
-
-plot(svmfit, dat)
-
-
-###
-
-# Tenemos más vectores de soporte
-
-
-length(svmfit$index)
-
-svmfit$index
-
-###
 
 # El siguiente comando indica que queremos comparar MVS con un kernel lineal, usando un rago de valores del parámetro `cost`
-
 set.seed(524)
-tune.out <- tune(svm, y~., data = dat, kernel = "linear", 
+tune.out <- tune(svm, Demented~MMSE + Age, data = dat2, kernel = "linear", 
                  ranges = list(cost = c(0.001, 0.01, 0.1, 1, 5, 10, 100)))
-
-
-###
-
 summary(tune.out)
-
-
-###
-
 # Elegimos el mejor modelo ajustado
-
-
 bestmod <- tune.out$best.model
-
-
-###
-
-
+bestmod
 summary(bestmod)
-
-
 ###
+plot(bestmod,dat2,ylim=c(14,31))
+# Como podemos observar, el modelo en su estado actual no puede mejorar demasiado con un kernel lineal
 
 # Ahora consideramos un conjunto de datos de prueba
-
-
-xtest <- matrix(rnorm(45*2), ncol = 2)
-ytest <- sample(c(-1, 1), 45, rep = TRUE)
-xtest[ytest == 1, ] <- xtest[ytest == 1, ] + 1
-testdat <- data.frame(x = xtest, y = as.factor(ytest))
-tail(testdat)
-
-
-###
-
-# Realizamos una clasificación usando el mejor modelo ajustado y el conjunto de datos de prueba. Luego, mostramos la matriz de confusión
-
-
-ypred <- predict(bestmod, testdat)
-table(predict = ypred, truth = testdat$y)
-
-
-###
-
-#### Máquinas de vectores de soporte
-
-# Generamos datos con una frontera de clase no lineal
-
-
-set.seed(6891)
-x <- matrix(rnorm(200*2), ncol = 2)
-x[1:100,] <- x[1:100,] + 2
-x[101:150,] <- x[101:150,] - 2
-y <- c(rep(1, 150), rep(2, 50))
-dat <- data.frame(x = x, y = as.factor(y))
-head(dat)
-
-
-###
-
-
-plot(x, col = y, pch = 16)
-
-
+set.seed(1536)
+dim(dat2)
+train <- sample(216,100)
+testdat2 <- dat2[train,];tail(testdat2)
+ypred <- predict(bestmod, testdat2)
+table(predict = ypred, truth = testdat2$Demented) / 100
+# Incluso este modelo sencillo tiene una precisión del ~85% para predecir la demencia en los pacientes
 ###
 
 # Generamos índices para el conjunto de entrenamiento
-
-train <- sample(200, 100)
+set.seed(1536)
+train <- sample(216, 100)
 tail(as.data.frame(train))
-
-
-###
-
 # Ajustamos una máquina de vectores de soporte con un kernel radial y valores de los parámetros `gamma = 1` y `cost = 1`
-
-svmfit <- svm(y~., data = dat[train, ], 
+svmfit <- svm(Demented~MMSE + Age, data = dat2[train, ], 
               kernel = "radial", gamma = 1, cost = 1)
-
-
-###
-
-
-plot(svmfit, dat[train, ])
-
-
-###
-
+plot(svmfit, dat2[train, ])
 summary(svmfit)
-
-###
+# El modelo obtenido al cambiar la función de kernel a 'radial' es increiblemente ajustado a nuestro
+# modelo. Esta gráfica es increíble para predecir la variable de interés, pero aun podemos intentar
+# mejorarla mediante la manipulación de las vartiables.
 
 # Ajustamos una máquina de vectores de soporte con un kernel radial y valores de los parámetros `gamma = 1` y `cost = 1e5`
-
-svmfit <- svm(y~., data = dat[train, ], 
+svmfit <- svm(Demented~MMSE + Age, data = dat2[train, ], 
               kernel = "radial", gamma = 1, cost = 1e5)
-
-
-###
-
-
-plot(svmfit, dat[train, ])
-
+plot(svmfit, dat2[train, ])
+# El modelo queda aun mejor! Quiza estamos acercandonos al punto de 'overfit' con el dataset
+# que utilizamos.
 
 ###
 
 # Ajustamos máquinas de vectores de soporte con un kernel radial para diferentes valores de los parámetros `cost` y `gamma`
 
 set.seed(1980)
-tune.out <- tune(svm, y~., data = dat[train, ], kernel = "radial", 
+train <- sample(216, 100)
+tune.out <- tune(svm, Demented~MMSE + Age, data = dat2[train, ], kernel = "radial", 
                  ranges = list(cost = c(0.1, 1, 10, 100, 1000), 
                                gamma = c(0.5, 1, 2, 3, 4)))
-
-
-###
-
 summary(tune.out)
-
-
-###
-
 # Realizamos clasificación con el mejor modelo ajustado y obtenemos la matriz de confusión.
-
-table(true = dat[-train, "y"], 
-      pred = predict(tune.out$best.model, newdata = dat[-train,]))
+table(true = dat2[-train, "Demented"], 
+      pred = predict(tune.out$best.model, newdata = dat2[-train,]))
+plot(tune.out$best.model, dat2[train, ])
 
